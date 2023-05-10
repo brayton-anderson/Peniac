ed5a0b08b3d9ed5912fd95096d21392b33396623
brayton-anderson <brayton@protonmail.com>
2023-05-11 01:48:52 +0300

    all changes done now

--- pages/youtubeplay.jsx
+++ pages/youtubeplay.jsx
@@ -5,9 +5,9 @@ const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
       width="100%"
-      height="100%"
+      height="100vh"
       src={`https://www.youtube.com/embed/${embedId}?autoplay=1&controls=0&showinfo=0&autohide=1&loop=1&mute=1`}
-      frameBorder="0"
+      //frameBorder="0"
       //allow="autoplay; autohide=1; loop=1; controls=0; showinfo=0; muted"
     //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
