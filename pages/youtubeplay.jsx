1e3be7a1180a74237f3aa3dae9e2602423e066c4
brayton-anderson <brayton@protonmail.com>
2023-05-11 01:46:59 +0300

    all changes done now

--- pages/youtubeplay.jsx
+++ pages/youtubeplay.jsx
@@ -4,11 +4,11 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
-      width="1080"
-      height="720"
+      width="100%"
+      height="100%"
       src={`https://www.youtube.com/embed/${embedId}?autoplay=1&controls=0&showinfo=0&autohide=1&loop=1&mute=1`}
       frameBorder="0"
-      allow="autoplay; autohide=1; loop=1; controls=0; showinfo=0; muted"
+      //allow="autoplay; autohide=1; loop=1; controls=0; showinfo=0; muted"
     //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
