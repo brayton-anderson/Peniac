dea989918fd3d710cfc20a2234138917989846f1
brayton-anderson <brayton@protonmail.com>
2023-05-11 01:44:15 +0300

    all changes done now

--- pages/youtubeplay.jsx
+++ pages/youtubeplay.jsx
@@ -8,7 +8,7 @@ const YoutubeEmbed = ({ embedId }) => (
       height="720"
       src={`https://www.youtube.com/embed/${embedId}?autoplay=1&controls=0&showinfo=0&autohide=1&loop=1&mute=1`}
       frameBorder="0"
-      allow="autoplay;"
+      allow="autoplay; autohide=1; loop=1; controls=0; showinfo=0; muted"
     //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
