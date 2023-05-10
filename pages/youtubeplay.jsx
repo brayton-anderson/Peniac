ce92f0425bd0bc8a7a97c2fbda06f2363842538a
brayton-anderson <brayton@protonmail.com>
2023-05-11 01:57:11 +0300

    all changes done now fast

--- pages/youtubeplay.jsx
+++ pages/youtubeplay.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
+  <div className="youtube-container">
     <iframe
       width="100%"
       height="100vh"
-      src={`https://www.youtube.com/embed/${embedId}?autoplay=1&controls=0&showinfo=0&autohide=1&loop=1&mute=1`}
+      src={`https://www.youtube.com/embed/${embedId}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1`}
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen
       //frameBorder="0"
       //allow="autoplay; autohide=1; loop=1; controls=0; showinfo=0; muted"
     //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
     />
   </div>
 );
