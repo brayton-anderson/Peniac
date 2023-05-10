60446984353b8e831da28601dd512fafbf0f79a8
brayton-anderson <brayton@protonmail.com>
2023-05-11 02:21:42 +0300

    all changes done now faster

--- pages/youtubeplay.jsx
+++ pages/youtubeplay.jsx
@@ -4,14 +4,15 @@ import styles from '../styles/homer.module.css'
 
 const YoutubeEmbed = ({ embedId }) => (
   <div className={styles.youtubeContainer}>
+    {/* <iframe width="942" height="530" src="https://www.youtube.com/embed/la3EJi-bLQ0" title="videoBg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
     <iframe
       id="frames"
      width="100vw"
       height="100vh"
-      src={`https://www.youtube.com/embed/${embedId}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1`}
+      src={`https://www.youtube.com/embed/la3EJi-bLQ0?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1`}
       title="YouTube video player" 
       frameborder="0" 
-      allow="accelerometer; autoplay; clipboard-write; loop; no-controls; encrypted-media; gyroscope; picture-in-picture" 
+      allow="accelerometer; autoplay; clipboard-write; looped; no-controls; controls-none encrypted-media; gyroscope; picture-in-picture" 
       allowfullscreen
       //frameBorder="0"
       //allow="autoplay; autohide=1; loop=1; controls=0; showinfo=0; muted"
