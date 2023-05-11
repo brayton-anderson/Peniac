9ba743fa493659e0cc204af509d42d800bddb3f9
brayton-anderson <brayton@protonmail.com>
2023-05-11 16:24:08 +0300

    all changes done now faster

--- pages/homer.jsx
+++ pages/homer.jsx
@@ -31,9 +31,9 @@ export default function Homer() {
             
             {/* SECTION 1 */}
             <div className={styles.containerInfo}>
-                {/* <div className={styles.overlay}></div> */}
-                {/* <video src="../assets/videoBg.mp4" autoPlay loop muted className={styles.video}/> */}
-                <YoutubeEmbed embedId="la3EJi-bLQ0" />
+                <div className={styles.overlay}></div>
+                <video src="../assets/videoBg.mp4" autoPlay loop muted className={styles.video}/>
+                {/* <YoutubeEmbed embedId="la3EJi-bLQ0" /> */}
                 
                 <div className={styles.content}>
                     <h1>ACCESS TO THE GLOBAL MARKET</h1>
